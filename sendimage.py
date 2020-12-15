8a15f39932c39c5f5350cd9cd1b5f269cf4032d9
N3998 <n3998@student.jamk.fi>
2020-12-15 14:33:48 +0200

    Update sendimage.py

--- sendimage.py
+++ sendimage.py
@@ -1,30 +1,26 @@
-from time import sleep
+
 from picamera import PiCamera
 import time
+import datetime
 import base64
-import paho.mqtt.publish as publish # vai client?
+import paho.mqtt.publish as publish
 
 timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-
-camera = PiCamera()
-camera.start_preview()
-    sleep(3)
-    camera.capture('/home/pi/Desktop/image.jpg', resize=(500, 281))
-    camera.close()
-    convertImageToBase64() #tarviiko tätä tehdä?
 
 def convertImageToBase64():
  with open("/home/pi/Desktop/image.jpg", "rb") as image_file:
      encoded = base64.b64encode(image_file.read())
  return encoded
 
+camera = PiCamera()
+camera.start_preview()
+sleep(3)
+camera.capture('/home/pi/Desktop/image.jpg', resize=(500, 281))
+camera.close()
+image64= convertImageToBase64()
 
-MQTT_SERVER = "192.168.9.40"  #Write Server IP Address
+
+MQTT_SERVER = "192.168.57.102"
 MQTT_PATH = "Image"
 
-f=open("i/home/pi/Desktop/image.jpg", "rb") 
-fileContent = f.read()
-byteArr = bytearray(fileContent)
-
-
-publish.single(MQTT_PATH, byteArr, hostname=MQTT_SERVER)
+publish.single(MQTT_PATH, image64, hostname=MQTT_SERVER, port=1883)
