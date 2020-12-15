b42aa95d5b88220340d68b0969531559773cc480
N3998 <n3998@student.jamk.fi>
2020-12-15 14:42:04 +0200

    Update kuva2.py

--- kuva2.py
+++ kuva2.py
@@ -1,26 +1,21 @@
 from picamera import PiCamera
 from time import sleep
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-import base64
-import paho.mqtt.client as mqtt
 
-client = mqtt.client()
-timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+sound = 23
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(sound, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
 
 camera = PiCamera()
 
-camera.start_preview()
-    sleep(5)
-    camera.capture('/home/pi/Desktop/image.jpg', resize=(500, 281))
-    camera.close()
-convertImageToBase64()
 
-def convertImageToBase64():
- with open("/home/pi/Desktop/image.jpg", "rb") as image_file:
- encoded = base64.b64encode(image_file.read())
- return encoded
+while True:
+    if GPIO.wait_for_edge(sound, GPIO.RISING):
+        camera.start_preview()
+        sleep(5)
+        camera.capture('/home/pi/Desktop/image.jpg')
+        camera.stop_preview()
+
